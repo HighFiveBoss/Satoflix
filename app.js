46f7e21ee49705435068a90c48927ad88be1ce8f
HighFiveBoss <yasiruslu07@gmail.com>
2023-12-04 22:55:49 +0300

    Squashed commit of the following:
    commit 88648366e3ecb060f2bda16eb853d33c72241294
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Mon Dec 4 20:40:49 2023 +0300
    
        Code simplified
    
    commit abb1ca58c8e0bbbccc88771f0d7103a7972efe0c
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Sun Dec 3 13:44:37 2023 +0300
    
        APIkey added
    
    commit 7e4bf00a4382af27c364b45b378fc8cfddb896de
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Sat Dec 2 17:21:52 2023 +0300
    
        website is working
    
    commit 9b8c34cb8b2486922538dec7686f02372d50cf8a
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Sat Dec 2 13:18:17 2023 +0300
    
        test
    
    commit 07ae506e90c2c09db36ad9f4ad7dcd9f94ba8c8c
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Fri Dec 1 14:50:16 2023 +0300
    
        rating fixed
    
    commit 82d0d21c09c55132453ec58ce03215f4a4758cf6
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Fri Dec 1 14:42:29 2023 +0300
    
        Fena oldu. filmler oneriyor falan hani
    
    commit b498a1914bf3f511e0a25a84e78dbb9a8876b9f8
    Merge: 4cf3a27 0630e42
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Thu Nov 30 18:22:33 2023 +0300
    
        Merge branch 'main' into backend
    
    commit 4cf3a276a6d87d6e53874ed029921fd11056d16c
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Thu Nov 30 17:35:45 2023 +0300
    
        modules added server started
    
    commit 4d2e1e3c5ab809ee04fd90881bb838d2ca6c3067
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Wed Nov 29 15:13:30 2023 +0300
    
        first commit to backend
    
    commit 0f7414b31c9070737ad114218de78ba8b2b207e7
    Author: HighFiveBoss <yasiruslu07@gmail.com>
    Date:   Wed Nov 29 15:05:53 2023 +0300
    
        npm installed

---
+++ app.js
@@ -0,0 +1,183 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const APIKey = '92c82bfb05msh7f5fda15c6bcfa4p104ebfjsnee15a1104b2d';
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+async function getUpcomingMovies(limit = 20) {
+    const today = new Date();
+    const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/get-coming-soon-movies',
+        params: {
+            homeCountry: 'US',
+            purchaseCountry: 'US',
+            currentCountry: 'US',
+            today: today.toISOString().slice(0, 10)
+        },
+        headers: {
+            'X-RapidAPI-Key': APIKey,
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data.slice(0 , limit);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getTopRatedMovies(limit = 20) {
+
+    const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/get-top-rated-movies',
+        headers: {
+            'X-RapidAPI-Key': APIKey,
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data.slice(0, limit);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getTopRatedSeries(limit = 20) {
+
+    const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/get-top-rated-tv-shows',
+        headers: {
+            'X-RapidAPI-Key': APIKey,
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data.slice(0, limit);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getDetails(id) {
+    const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/get-overview-details',
+        params: {
+            tconst: id,
+            currentCountry: 'US'
+        },
+        headers: {
+            'X-RapidAPI-Key': APIKey,
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+async function getMoreLikeThis(id, limit = 10) {
+    const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/get-more-like-this',
+        params: {
+            tconst: id
+        },
+        headers: {
+            'X-RapidAPI-Key': APIKey,
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data.slice(0, limit);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//randomMovieImageLink1 randomMovie1 randomMovieDate1 randomMovieResolution1 randomMovieDurationTime1 randomMovieRating1
+
+app.get("/", async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies(4),
+        topRatedMovies = await getTopRatedMovies(8),
+        topRatedSeries = await getTopRatedSeries(4);
+
+
+    const upcomingMoviesIds = upcomingMovies.map(item => item.id.slice(7, -1)),
+    topRatedMoviesIds = topRatedMovies.map(item => item.id.slice(7, -1)),
+    topRatedSeriesIds = topRatedSeries.map(item => item.id.slice(7, -1));
+
+    const upcomingMoviesDetails = [];
+
+    for (const upcomingMovieId of upcomingMoviesIds) {
+        const { id, title: { image: { url }, title, year } } = await getDetails(upcomingMovieId); 
+        upcomingMoviesDetails.push({ id, url, title, year });
+    };
+
+    const topRatedMoviesDetails = [];
+
+    for (const topRatedMovieId of topRatedMoviesIds) {
+        const { id, title: { image: { url }, runningTimeInMinutes, title, year }, ratings: { rating } } = await getDetails(topRatedMovieId);
+        topRatedMoviesDetails.push({ id, url, title, year, runningTimeInMinutes, rating});
+    };
+
+    const topRatedSeriesDetails = [];
+
+    for (const topRatedSeriesId of topRatedSeriesIds) {
+        const { id, title: { image: { url }, runningTimeInMinutes, title, year}, ratings: { rating } } = await getDetails(topRatedSeriesId);
+        topRatedSeriesDetails.push({ id, url, runningTimeInMinutes, title, year, rating});
+    };
+
+
+
+    res.render("index.ejs", {
+        upcomingMovies: upcomingMoviesDetails,
+        topRatedMovies: topRatedMoviesDetails,
+        topRatedSeries: topRatedSeriesDetails
+    });
+});
+
+app.post("/movie-details.ejs", async (req, res) => {
+    const id = req.body["movieId"];
+
+    const movieDetails = await getDetails(id);
+
+    const relatedMoviesIds = await getMoreLikeThis(id, 4);
+
+    const relatedMoviesDetails = [];
+    for (const relatedMoviesId of relatedMoviesIds) {
+        const { id, title: { image: { url }, runningTimeInMinutes, title, year}, ratings: { rating } } = await getDetails(relatedMoviesId.slice(7, -1));
+        relatedMoviesDetails.push({ id, url, runningTimeInMinutes, title, year, rating});
+    };
+
+
+    res.render("movie-details.ejs", {
+        movieDetails: movieDetails,
+        relatedMoviesDetails: relatedMoviesDetails
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
--- script.js
+++ script.js
@@ -50,3 +50,5 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
+
+
